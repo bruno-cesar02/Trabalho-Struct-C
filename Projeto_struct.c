68ba04b72e04513a44081896a65d7e34d2342af2
Lieberr <gustavoliebfigueira@gmail.com>
2025-04-30 08:29:47 -0300

    Comit gustavo lieb

--- Projeto_struct.c
+++ Projeto_struct.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// Criar struct
 struct Biblioteca {
     char titulo[100];
     char autor[100];
@@ -12,9 +13,12 @@ struct Biblioteca {
     int total_emprestimos;
 };
 
+// Variavel global para o tamanho do vetor
 struct Biblioteca total_livros[100];
 int tamanho_livros = 0;
 
+
+// Função cadastrar
 void Cadastrar() {
     int novo_id;
     printf("Digite o id: ");
@@ -47,13 +51,14 @@ void Cadastrar() {
     tamanho_livros++;
 }
 
+// Busca livro pelo ID
 void Consultar() {
-    int id;
+    int id_con;
     printf("Digite o ID desejado: ");
-    scanf("%d", &id);
+    scanf("%d", &id_con);
 
     for (int i = 0; i < tamanho_livros; i++) {
-        if (total_livros[i].id == id) {
+        if (total_livros[i].id == id_con) {
             printf("Titulo: %s\n", total_livros[i].titulo);
             printf("Autor: %s\n", total_livros[i].autor);
             printf("Paginas: %d\n", total_livros[i].numero_pag);
@@ -64,9 +69,10 @@ void Consultar() {
         }
     }
 
-    printf("Livro com ID %d nao encontrado.\n", id);
+    printf("Livro com ID %d nao encontrado.\n", id_con);
 }
 
+// Mostra todos os livros cadastrados
 void Listar() {
     if (tamanho_livros == 0) {
         printf("Nenhum livro cadastrado.\n");
@@ -83,6 +89,101 @@ void Listar() {
     }
 }
 
+// Função para realizar o emprestimo
+void Emprestar() {
+
+    if (tamanho_livros == 0) {
+        printf("Nenhum livro cadastrado.\n");
+        return;
+    }
+
+    char nome_emprestimo[100];
+    int id_emp;
+    bool encontrado = false;
+    
+    printf ("Digite o ID do livro procurado: ");
+    scanf ("%d", &id_emp);
+
+    for (int i = 0;i < tamanho_livros; i++) {
+        if (total_livros[i].id == id_emp) {
+            encontrado = true;
+
+
+            if (total_livros[i].qntd > 0) {
+                printf ("Nome da pessoa: ");
+                scanf (" %[^\n]", nome_emprestimo);
+
+                for (int j = 0; j < 10; j++){
+                    if (strlen(total_livros[i].emprestimo[j]) == 0) {
+                        strcpy(total_livros[i].emprestimo[j], nome_emprestimo);
+                        total_livros[i].qntd--;
+                        printf ("Livro emprestado com sucesso!\n");
+                        return;
+                    }
+                }
+
+                printf ("Total de livros para emprestimo atingido.\n");
+
+            }
+
+            else {
+                printf ("Nao ha quantidade de livros suficiente para emprestimo.");
+            }
+        }
+
+    }
+    
+    if (encontrado == false) {
+        printf ("ID %d nao encontrado.", id_emp);
+    }
+
+}
+
+void Devolver () {
+
+    if (tamanho_livros == 0) {
+        printf ("Nao ha livros cadastrados.\n");
+        return;
+    }
+
+    int id_devo;
+    bool encontrou = false;
+    char nome_devolucao[100];
+
+    printf ("ID do livro: \n");
+    scanf ("%d", &id_devo);
+
+    for (int i = 0; i < tamanho_livros; i++) {
+        if (total_livros[i].id == id_devo) {
+            encontrou = true;
+
+            printf ("Nome da pessoa que emprestou: \n");
+            scanf (" %[^\n]", nome_devolucao);
+
+            for (int j = 0; j < 10; j ++) {
+                //compara nomes, se for igual retorna 0
+                if (strcmp(total_livros[i].emprestimo[j], nome_devolucao) == 0) {
+                    //se nome for encontrado limpa a posição
+                    strcpy(total_livros[i].emprestimo[j], "");
+                    total_livros[i].qntd++;
+                    printf ("Livro devolvido com sucesso!\n");
+                    return;
+                }
+            }
+
+            printf ("Nome nao encontrado na lista de emprestimos.\n");
+            return;
+
+        }
+    }
+
+    if (encontrou == false) {
+        printf ("ID nao encontrado.\n");
+    }
+
+}
+
+// Menu
 int main() {
     int opcao;
 
